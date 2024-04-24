9c2caeaf6884a7c3d874b5b3604700f88cc0779d
dbrasher <dbrasher5@ivytech.edu>
2024-04-24 15:16:14 -0400

    Generalize dummy data generation, separating django-compatible script runner to its own file for any use.

---
+++ create_dummy_users.py
@@ -0,0 +1,7 @@
+from users.models import IvyMeshUser
+
+
+for role, m, f in [("admin", 9, IvyMeshUser.objects.create_superuser), ("staff", 19, IvyMeshUser.objects.create_user), ("student", 99, IvyMeshUser.objects.create_user)]:
+    for n in range(m):
+        f(username=f"{role}_user{n}", email=f"{role}_user{n}@fake.fake", password=f"{role}_user{n}_1234", role=role)
+
