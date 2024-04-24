8f12117f00dd02e5a1c544ee1a6ff184b3ec7617
dbrasher <dbrasher5@ivytech.edu>
2024-04-24 12:47:03 -0400

    Fix Django config missing in dumm user script, take 4.

---
+++ create_dummy_users.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+python3 manage.py shell --command 'from users.models import IvyMeshUser; for role, m in [("admin", 9), ("staff", 19), ("student", 99)]: for n in range(m): func = IvyMeshUser.objects.create_superuser if role == "admin" else IvyMeshUser.objects.create_user; func(username=f"{role}_user{n}", email=f"{role}_user{n}@fake.fake", password=f"{role}_user{n}_1234", role=role)'
+
