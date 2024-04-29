ca30c65f5bae679582a03c0c1a07391244defda4
carlos <clizarazulinare@ivytech.edu>
2024-04-29 12:24:34 -0400

    Carlos update(html,css,models)

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db import models # type: ignore
+from django.utils import timezone # type: ignore
 
 # Create your models here.
+
+#The following clas is for the information of the courses
+class Post(models.Model):
+    course_id = models.BigAutoField(primary_key=True)
+    course_name = models.CharField(max_length=10)
+    instruction_mode = models.CharField(max_length=12)
+    part_of_term = models.CharField(max_length=15)
+    credits = models.IntegerField()
+    campus = models.CharField(max_length=20)
+    days = models.DateTimeField()
+    location =  models.CharField(max_length=10)
